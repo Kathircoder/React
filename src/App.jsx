57c02dc6ed4192773eb6aa2cffc7362230b28ebc
Kathircoder <bkathiravan01@gmail.com>
2024-09-16 14:44:12 +0530

    kathir coder

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ const App = () => {
         <>
         <div className="h-screen w-screen fleex justify center items-center bg-purple-600">
             <div clasName=" h-3/6 w-[30%] flex justify-center items-center bg-white rounded-md shadow-2xl">
+            {/* // */}
             <form className="w-[80%] h-[80%] flex flex-col justify-center items-center gap-4">
                  <input type="text" name="" id="" placeholder="Name" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500"/>
             </form>
